d55ea297fb28551e3ffc732ad6764c0df1f12374
Ulysse ARNAUD <mexanga@gmail.com>
2022-12-20 22:39:31 +0100

    May solve workflows

--- bin/install.cmd
+++ bin/install.cmd
@@ -3,8 +3,8 @@ setlocal EnableDelayedExpansion
 
 wget https://github.com/puppeteer/puppeteer/archive/refs/tags/puppeteer-core-v19.4.1.tar.gz -P libs
 powershell -Command "if (!(Test-Path libs/puppeteer)) { New-Item -ItemType Directory -Path libs/puppeteer }" 2> nul
-tar -xkf libs\puppeteer-core-v19.4.1.tar.gz -C libs/puppeteer
-del libs\puppeteer-core-v19.4.1.tar.gz
+tar -xkf libs/puppeteer-core-v19.4.1.tar.gz -C libs/puppeteer
+del libs/puppeteer-core-v19.4.1.tar.gz
 cd libs/puppeteer/puppeteer-puppeteer-core-v19.4.1
 powershell -Command "(Get-Content package.json) -replace 'husky install','' | Set-Content package.json" 2> nul
 npm install --silent
